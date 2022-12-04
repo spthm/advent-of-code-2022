91b68e063a11696b1baf714a538392c7313702e2
Sam Thomson <2648269+spthm@users.noreply.github.com>
2022-12-04 13:07:20 +0000

    Speed up day 3. Linear search ~40x faster than constructing HashSet

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,4 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use std::collections::HashSet;
 
 #[aoc_generator(day3)]
 pub fn parse(input: &str) -> Vec<String> {
@@ -12,8 +11,8 @@ pub fn part1(parsed: &Vec<String>) -> u32 {
         .iter()
         .map(|rucksack| {
             let (first, second) = rucksack.split_at(rucksack.len() / 2);
-            let c = common(first, second);
-            priority(c.as_bytes()[0])
+            let c = first.chars().find(|c| second.contains(*c)).unwrap();
+            priority(c)
         })
         .sum()
 }
@@ -21,28 +20,23 @@ pub fn part1(parsed: &Vec<String>) -> u32 {
 #[aoc(day3, part2)]
 pub fn part2(parsed: &Vec<String>) -> u32 {
     parsed
-        .chunks(3)
+        .chunks_exact(3)
         .map(|group| {
-            let mut c = common(group[0].as_str(), group[1].as_str());
-            c = common(c.as_str(), group[2].as_str());
-            priority(c.as_bytes()[0])
+            let c = group[0]
+                .chars()
+                .find(|c| group[1].contains(*c) && group[2].contains(*c))
+                .unwrap();
+            priority(c)
         })
         .sum()
 }
 
-fn common(first: &str, second: &str) -> String {
-    let fs: HashSet<char> = HashSet::from_iter(first.chars());
-    let ss: HashSet<char> = HashSet::from_iter(second.chars());
-
-    return fs.intersection(&ss).collect::<String>()
-}
-
-fn priority(c: u8) -> u32 {
+fn priority(c: char) -> u32 {
     assert!(c.is_ascii());
 
-    match c as char {
+    match c {
         'a'..='z' => (c as u32) - ('a' as u32) + 1,
         'A'..='Z' => (c as u32) - ('A' as u32) + 27,
-        _ => panic!("{} not in [a - z][A - Z]", c)
+        _ => panic!("{} not in [a - z][A - Z]", c),
     }
 }
